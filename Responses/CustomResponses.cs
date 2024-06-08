1175ba5d8248918a9b360383a3f1bd3a93ebcff4
joakimdahl@gmx.us <joakimdahl@gmx.us>
2024-06-08 13:45:50 -0700

    dark theme

--- Responses/CustomResponses.cs
+++ Responses/CustomResponses.cs
@@ -1,8 +1,13 @@
-﻿namespace PwdMngrWasm.Responses
+﻿using PwdMngrWasm.DTOs;
+
+namespace PwdMngrWasm.Responses
 {
     public class CustomResponses
     {
         public record RegistrationResponse(bool Flag = false, string Message = null!);
         public record LoginResponse(bool Flag = false, string Message = null!, string JwtToken = null!, string RefreshToken = null!);
+        public record ApiResponse(bool Flag = false, string Message = null!, object payload = null!);
+        public record GetAllPasswordsApiResonse(bool Flag = false, string Message = null!, List<PasswordEntry> PasswordEntries = null!);
+
     }
 }
