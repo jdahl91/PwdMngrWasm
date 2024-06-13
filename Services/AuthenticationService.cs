f528c2e255578fee987907a74795e21c163b4ed7
joakimdahl@gmx.us <joakimdahl@gmx.us>
2024-06-12 18:05:32 -0700

    more progress

--- Services/AuthenticationService.cs
+++ Services/AuthenticationService.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.JSInterop;
 using PwdMngrWasm.DTOs;
@@ -25,8 +26,12 @@ namespace PwdMngrWasm.Services
             _authenticationStateProvider = (CustomAuthenticationStateProvider)authenticationStateProvider;
         }
 
-        public async Task<LoginResponse> Login(LoginDTO form)
+        public async Task<bool> LoginAsync(LoginDTO form)
         {
+            var customAuthenticationStateProvider = _authenticationStateProvider;
+            await customAuthenticationStateProvider.MarkUserAsAuthenticated("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJOYW1lIjoiSm9ha2ltIiwiRW1haWwiOiJqb2FraW1kYWhsQGdteC51cyIsIlJvbGUiOiJBZG1pbiJ9.xwehf7zL11t0lHZhSynNmeQsYZghMdDFoAWNPrYcfhM", "new-v9-refresh-token-test");
+            return true;
+
             //var response = await _httpClient.PostAsJsonAsync("https://example.com/api/auth/login", new { username, password });
             //if (!response.IsSuccessStatusCode) return false;
 
@@ -35,10 +40,10 @@ namespace PwdMngrWasm.Services
             //((CustomAuthenticationStateProvider)_authenticationStateProvider).MarkUserAsAuthenticated(username);
             //_httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
-            return new LoginResponse();
+            return true;
         }
 
-        public async Task Logout()
+        public async Task LogoutAsync()
         {
             //var response = await _httpClient.PostAsJsonAsync("https://example.com/api/auth/logout");
             //await _jsRuntime.InvokeVoidAsync("localStorage.removeItem", "authToken");
